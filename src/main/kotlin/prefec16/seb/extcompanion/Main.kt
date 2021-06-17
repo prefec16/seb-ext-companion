c0b5cb6b1f9e985693c624e7b46c01781eb0e09d
prefec16 <prefec16@new.edvhtl.at>
2021-06-17 13:34:10 +0200

    cleanup code

--- src/main/kotlin/prefec16/seb/extcompanion/Main.kt
+++ src/main/kotlin/prefec16/seb/extcompanion/Main.kt
@@ -13,7 +13,8 @@ const val ORIGINATOR_VERSION_KEY = "originatorVersion"
 const val BROWSER_EXAM_KEY_SALT_KEY = "examKeySalt"
 
 const val SEB_USERAGENT_VERSION = "SEB/1.0.0.0"
-const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 $SEB_USERAGENT_VERSION"
+const val DEFAULT_USER_AGENT =
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 $SEB_USERAGENT_VERSION"
 
 @ExperimentalUnsignedTypes
 fun main(args: Array<String>) {
@@ -30,13 +31,19 @@ fun main(args: Array<String>) {
             val config = ConfigExtractor.parse(body.config)
             //idk why, but this element will not be serialized to json in the original version
             config.remove(ORIGINATOR_VERSION_KEY)
-            val examKeySalt = config.getIfHas(BROWSER_EXAM_KEY_SALT_KEY)?.asString?.replace("\"", "")
+            val examKeySalt = config.getIfHas(BROWSER_EXAM_KEY_SALT_KEY)?.asJsonPrimitive?.asString
 
             val configKey = Crypto.computeConfigurationKey(config)
             val browserExamKey = Crypto.computeBrowserExamKey(configKey, examKeySalt)
 
             println("Config successfully parsed!")
-            ctx.status(200).json(mapOf("configKey" to configKey, "browserExamKey" to browserExamKey, "userAgent" to getUserAgent(config)))
+            ctx.status(200).json(
+                mapOf(
+                    "configKey" to configKey,
+                    "browserExamKey" to browserExamKey,
+                    "userAgent" to getUserAgent(config)
+                )
+            )
         }
     }
 
@@ -45,7 +52,7 @@ fun main(args: Array<String>) {
             val requestHash = Crypto.computeRequestHash(body.url, body.browserExamKey)
             val configKeyHash = Crypto.computeConfigKeyHash(body.url, body.configKey)
 
-            if(printHashes){
+            if (printHashes) {
                 println("calculated requestHash $requestHash and configKeyHash $configKeyHash for ${body.url}")
             }
 
@@ -71,6 +78,4 @@ data class CalculateHeadersBody(val url: String, val browserExamKey: String, val
 
 data class ConfigKeyBody(val config: String)
 
-fun JsonObject.getIfHas(key: String): JsonElement? {
-    return if (this.has(key)) this.get(key) else null
-}
+fun JsonObject.getIfHas(key: String) = if (this.has(key)) this.get(key) else null
